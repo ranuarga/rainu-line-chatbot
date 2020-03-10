0f6b8f7d56d4b5547c8da024c90c54014dfad330
ranuarga <august.arga@gmail.com>
2020-03-10 10:20:54 +0700

    Update: Fix Naming Typo

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -138,7 +138,7 @@ class Webhook extends Controller
         $message = 'Fitur ini belum jadi';
         $response = $this->bot->getMessageContent($event['message']['id']);
         if ($response->isSucceeded()) {
-            file_put_contents('php://stderr', 'Body: ' . $request->getRawBody());
+            file_put_contents('php://stderr', 'Body: ' . $response->getRawBody());
             $img = \Cloudinary\Uploader::upload('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==');
             $res = $this->client->request('POST', 'https://trace.moe/api/search?url=' . $img['secure_url'])->getBody()->getContents();
             
